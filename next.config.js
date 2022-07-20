387dc08cda810e1ae8665c99bec90795c8e02be2
Karan Kumar Gupta <Karangupta9615@gmail.com>
2022-07-20 06:22:45 +0530

    next.config.js updated

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
-// module.exports = nextConfig
+module.exports = nextConfig
 
-module.exports = {
-  async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'https://api.example.com/:path*',
-        },
-      ]
-    },
-};
+// module.exports = {
+//   async rewrites() {
+//       return [
+//         {
+//           source: '/api/:path*',
+//           destination: 'https://api.example.com/:path*',
+//         },
+//       ]
+//     },
+// };
